62ae02398734e79c31713064777485387a69cf2b
Sandeep <das.sandeep2312@gmail.com>
2022-03-16 14:26:11 +0530

    Added Structure of new patient

--- functions.h
+++ functions.h
@@ -2,6 +2,22 @@ char reception_staff1[] = "reception1", reception_pass1[] = "password"; // sampl
 char doctor1[] = "doctor1", doctor_pass1[] = "password";                // sample username and password for doctor
 char ipd_staff1[] = "ipd1", ipd_pass1[] = "password";                   // sample username and password for ipd staff
 char username_input[20], password_imput[20];                            // username and password input by the user
+/*--------------------------------------------------------------------------------------*/
+
+struct new_patient
+{
+    char name[50];
+    int age ;
+    int height ; //in cm
+    int weight ; // in kg
+    char blood_group[10];
+    char gender[10];
+    char IBS[200];
+    int Contact_no ;
+    char Relation[50];
+    char health_cond[20];
+    char Previous_med_history[200];
+};
 
 /* ------------------------------------------------------------------------------------- */
 
@@ -28,5 +44,6 @@ int login()
         return 3;
     }
 }
+
 
 /* ------------------------------------------------------------------------------------- */
